4dec5801ffe971b83f71b638565905339fe98ac5
Akpriest <Akpriest@192.168.0.7>
2016-05-04 21:00:46 -0400

    updated int and array junit tests

--- MiniGolf/src/testMiniGolf/scoredetialTest.java
+++ MiniGolf/src/testMiniGolf/scoredetialTest.java
@@ -11,8 +11,9 @@ import org.junit.Test;
    public void test() {
       Round test = new Round();
       
+      int [] ArrayTest = {1,2,3,4};
       int[] Array = test.scoredetial();
-    
+      assertArrayEquals(Array, ArrayTest);
     
    }
  }
