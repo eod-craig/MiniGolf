19001c865aa9cd1bf072560af237091c2b4dfe2f
Akpriest <Akpriest@192.168.0.7>
2016-05-04 20:20:30 -0400

    added new files with junit test

---
+++ MiniGolf/src/testMiniGolf/scoredetialTest.java
@@ -0,0 +1,18 @@
+package testMiniGolf;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+ 
+
+ public class scoredetialTest{
+
+ @Test
+   public void test() {
+      Round test = new Round();
+      
+      int[] Array = test.scoredetial();
+    
+    
+   }
+ }
